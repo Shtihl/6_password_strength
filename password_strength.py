8bf131128e5592bf938c55651925f3daa6799ee8
Shtihl <shtihlfd@yandex.ru>
2017-12-12 10:37:03 +0300

    some refactor

--- password_strength.py
+++ password_strength.py
@@ -7,23 +7,37 @@ def load_black_list():
         return black_list_passwords.read().splitlines()
 
 
-def get_password_strength(password, black_list):
-    strength_points = 1
-    unique_symbol_rate = 0.8
-    if password in black_list or len(password) < 6:
-        return strength_points
-    if len(set(password)) > len(password) * unique_symbol_rate:
-        strength_points += 3
+def find_symbol_in_password(password):
+    result = 0
     for reg_exp in (r'\d',
                     r'[a-z]',
                     r'[A-Z]',
                     r'[~!@#$%^&*()_+`\-={}[\]:;<>./\\]'):
         if re.search(reg_exp, password):
-            strength_points += 1
+            result += 1
+    return result
+
+
+def password_length(password):
+    result = 0
+    unique_symbol_rate = 0.8
+    if len(set(password)) > len(password) * unique_symbol_rate:
+        result += 3
     for length in [6, 10]:
         if len(password) >= length:
-            strength_points += 1
-    return strength_points
+            result += 1
+    return result
+
+
+def get_password_strength(password, black_list):
+    strength_points = 1
+    if password in black_list or len(password) < 6:
+        return strength_points
+    else:
+        strength_points = strength_points + \
+                          password_length(password) + \
+                          find_symbol_in_password(password)
+        return strength_points
 
 
 if __name__ == '__main__':
